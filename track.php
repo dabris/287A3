06de7940e55ace4ed9c2f20f573e63b5c0d6ce4b
dabris <dabris16.x@gmail.com>
2018-04-13 16:00:36 -0400

    done

--- track.php
+++ track.php
@@ -6,15 +6,21 @@
 </head>
     <body>
     <?php
-        $inOneDay=60*60*24+time();
-        setCookie('visited', date("G:I-m/d/y"),$inOneDay);
-        if(isset($_COOKIE['visited'])){
-          setCookie('count',(int)&_COOKIE['count']+1,$inOneDay);
-            echo "Hello, this is your $count time here";
+        date_default_timezone_set("America/New_York");
+        if(isset($_COOKIE['count'])){
+            $inOneDay=60*60*24+time();
+          setcookie('count',$_COOKIE['count']+1,$inOneDay);
+            echo "Hello, this is your #".$_COOKIE['count']." time here.<br>The last time you visited this website was: ". $_COOKIE['visitTime'];
+           
         }else{
+             $inOneDay=60*60*24+time();
+            setcookie('count',2,$inOneDay);
             echo"Welcome! You are a new customer here";
-            $count++;
+           
+          
         }
+        
+        setCookie('visitTime', date("r"),$inOneDay);
         ?>
     </body>
 </html>
